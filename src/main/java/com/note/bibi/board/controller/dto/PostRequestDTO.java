496d91b045b1466aec526bf150ad9c8cbc013788
Hazel0c0 <secretwish5@naver.com>
2023-11-19 16:05:28 +0900

    Refactor. post 유효성 검사

--- src/main/java/com/note/bibi/board/controller/dto/PostRequestDTO.java
+++ src/main/java/com/note/bibi/board/controller/dto/PostRequestDTO.java
@@ -1,9 +1,16 @@
 package com.note.bibi.board.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PostRequestDTO {
+  @NotBlank(message = "제목을 입력해주세요")
+  @Size(min = 1, max = 30, message = "제목은 1글자 이상 30글자 이하여야 합니다")
   private String title;
+
+  @NotBlank(message = "내용을 입력해주세요")
+  @Size(min = 1, max = 1000, message = "내용은 1글자 이상 1000글자 이하여야 합니다")
   private String content;
 }
