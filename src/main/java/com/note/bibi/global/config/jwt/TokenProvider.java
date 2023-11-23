470687ac4e03fb6609de100a055932cb8f361a1f
Hazel0c0 <secretwish5@naver.com>
2023-11-23 14:48:24 +0900

    style. TokenProvider.createToken() 토큰 생성 메서드

--- src/main/java/com/note/bibi/global/config/jwt/TokenProvider.java
+++ src/main/java/com/note/bibi/global/config/jwt/TokenProvider.java
@@ -15,9 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -40,18 +38,25 @@ public class TokenProvider {
     );
     System.out.println("expiryDate = " + expiryDate);
 
+    // 추가 클레임 정의
+    Map<String, Object> claims = new HashMap<>();
+    claims.put("email", user.getEmail());
+
     return Jwts.builder()
+        // token header에 들어갈 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
+        .signWith(
+            tokenInfo.getSecretKey()
+            , SignatureAlgorithm.HS256
+        )
         .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 type : JWT
+        .setClaims(claims) // 추가클레임은 먼저 설정해야 함
+        // 추가하는 클레임이 이미 토큰에 포함된 다른 정보들과 충돌하지 않도록 해야하기 때문
         .setIssuer(tokenInfo.getIssuer()) // 내용 iss
         .setIssuedAt(new Date()) // 내용 iat : 토큰 발급 시간
         .setExpiration(expiryDate) // 내용 exp : 유효 시간
-        .setSubject(user.getEmail()) // 내용 sub : 유저의 이메일
-        .claim("ID", user.getId()) // 클레임 id : 유저 ID
-        // 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
-        .signWith(tokenInfo.getSecretKey(), SignatureAlgorithm.HS256)
+        .setSubject(user.getId()) // sub: 토큰을 식별할 수 있는 주요데이터
         .compact();
   }
-
 
   /**
    * 토큰 유효성 검사 : 클라이언트가 보낸 토큰을 디코딩 / 파싱해서 토큰의 위조 여부 확인
