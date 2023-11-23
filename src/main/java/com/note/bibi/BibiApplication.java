44a59ba80bc934076a03fcde4d8bf5f2dd3c1299
Hazel0c0 <secretwish5@naver.com>
2023-11-23 17:24:53 +0900

    error. security 정지

--- src/main/java/com/note/bibi/BibiApplication.java
+++ src/main/java/com/note/bibi/BibiApplication.java
@@ -3,11 +3,14 @@ package com.note.bibi;
 import com.note.bibi.global.config.jwt.TokenInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-@SpringBootApplication
+
+//@SpringBootApplication
 @EnableJpaAuditing // JPA Auditing 활성화
 @EnableConfigurationProperties(TokenInfo.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Security 기능 끄기
 public class BibiApplication {
 
 	public static void main(String[] args) {
