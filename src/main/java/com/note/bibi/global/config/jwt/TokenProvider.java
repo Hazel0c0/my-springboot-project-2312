89f5d345d8417b0cc8683632db6b5f9b025eab91
Hazel0c0 <secretwish5@naver.com>
2023-11-23 15:04:05 +0900

    Refactor. TokenProvider.validateToken() @return : boolean

--- src/main/java/com/note/bibi/global/config/jwt/TokenProvider.java
+++ src/main/java/com/note/bibi/global/config/jwt/TokenProvider.java
@@ -15,7 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +27,7 @@ public class TokenProvider {
 
   /**
    * JWT 토큰 생성 메서드
+   *
    * @param user : 토큰 내용에 포함될 로그인 유저 정보
    * @return : 생성된 Json 을 암호화한 토큰 값
    */
@@ -38,31 +41,19 @@ public class TokenProvider {
     );
     System.out.println("expiryDate = " + expiryDate);
 
-    // 추가 클레임 정의
-    Map<String, Object> claims = new HashMap<>();
-    claims.put("email", user.getEmail());
-
     return Jwts.builder()
-        // token header에 들어갈 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
-        .signWith(
-            tokenInfo.getSecretKey()
-            , SignatureAlgorithm.HS256
-        )
         .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 type : JWT
-        .setClaims(claims) // 추가클레임은 먼저 설정해야 함
-        // 추가하는 클레임이 이미 토큰에 포함된 다른 정보들과 충돌하지 않도록 해야하기 때문
         .setIssuer(tokenInfo.getIssuer()) // 내용 iss
         .setIssuedAt(new Date()) // 내용 iat : 토큰 발급 시간
         .setExpiration(expiryDate) // 내용 exp : 유효 시간
-        .setSubject(user.getId()) // sub: 토큰을 식별할 수 있는 주요데이터
+        .setSubject(user.getEmail()) // 내용 sub : 유저의 이메일
+        .claim("ID", user.getId()) // 클레임 id : 유저 ID
+        // 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
+        .signWith(tokenInfo.getSecretKey(), SignatureAlgorithm.HS256)
         .compact();
   }
 
-  /**
-   * 토큰 유효성 검사 : 클라이언트가 보낸 토큰을 디코딩 / 파싱해서 토큰의 위조 여부 확인
-   * @param token - 클라이언트가 전송한 인코딩 된 토큰
-   * @return - 토큰에서 subject(userIdx, 유저 식별자) 를 꺼내서 반환
-   */
+  // 토큰 유효성 검사
   public boolean validateToken(String token) {
     try {
       Jwts.parser()
